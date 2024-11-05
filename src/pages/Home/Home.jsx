3dcf93e1cac069108985329dba6db21788b378a2
bren112 <brenomarinho1610@gmail.com>
2024-11-05 00:12:10 -0300

    Pv Festfy Reveillon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import './Home.css';
-import banner from './b.png';
+import banner from './org.png';
 
 function Home() {
     return (
@@ -21,7 +21,7 @@ function Home() {
                 <br />
 
                 <div className="dir">
-                    <img id='banner' src={banner} alt="Banner" />
+                    <img src={banner} alt="Banner" />
                     <Link to="/pagamento">
                         <button>QUERO IR!</button>
                     </Link>
