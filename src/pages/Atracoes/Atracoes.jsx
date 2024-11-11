770c9c9691d41a06f8f5345e7715bd0db5ad14d0
bren112 <brenomarinho1610@gmail.com>
2024-11-11 01:06:51 -0300

    Dj Moi

--- src/pages/Atracoes/Atracoes.jsx
+++ src/pages/Atracoes/Atracoes.jsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './Atracoes.css';
-import ctc from './ctc.png';
-import img from './img.png';
-import paredao from './paredao.png';
-import kury from './djkury2.png'
-import kurydois from './djkuri.png';
-import palco from './palco2.png';
-import virtu from './virtu.png';
-
+import djmoi from './moi2.png';
 
 function Atracoes() {
     const [progress, setProgress] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controle do modal
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -20,12 +14,44 @@ function Atracoes() {
         return () => clearInterval(interval); 
     }, []);
 
+    // Função para abrir o modal
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    // Função para fechar o modal
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className="atracoes">
-             <br />
-        <h1 id='title'>
-        EM BREVE</h1>
-            <br/>
+            <br />
+            <h1 id="title">DJ MOI</h1>
+            <br />
+            
+            {/* Imagem que abre o modal ao ser clicada */}
+            <img 
+                id="imgsobre" 
+                src={djmoi} 
+                alt="DJ Moi" 
+                onClick={openModal} 
+                style={{ cursor: 'pointer' }} 
+            />
+
+            {/* Modal */}
+            {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <div className="modal-content" onClick={e => e.stopPropagation()}>
+                        <img 
+                            src={djmoi} 
+                            alt="DJ Moi" 
+                            style={{ width: '100%', height: 'auto' }} 
+                        />
+                    </div>
+                </div>
+            )}
+
             <div className="loading-circle">
                 <div className="circle"></div>
             </div>
